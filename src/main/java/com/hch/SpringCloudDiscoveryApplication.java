c0e7564c8760738ad6cf638d53bc6ae99be04ada
Hudson Henrique Lopes <hudsonhenriquelopes@gmail.com>
2026-05-21 15:41:09 -0300

    Fix run

--- src/main/java/com/hch/SpringCloudDiscoveryApplication.java
+++ src/main/java/com/hch/SpringCloudDiscoveryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class SpringCloudApplication {
+public class SpringCloudDiscoveryApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudApplication.class, args);
+        SpringApplication.run(SpringCloudDiscoveryApplication.class, args);
     }
 }
